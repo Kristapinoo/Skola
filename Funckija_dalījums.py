5cb17df28af465754cd0e992a1bc9f7c45cd8ff5
Kristapinoo <97234596+Kristapinoo@users.noreply.github.com>
2022-01-13 15:51:01 +0200

    Add files via upload

---
+++ Funckija_dalījums.py
@@ -0,0 +1,5 @@
+def izt(a,b=2):
+    return a/b
+print(izt(13,5)) # ņems šos abus skaitļus
+print(izt(13)) # 13 būs a skaitlis, jo b ir dots
+print(izt(b=7, a=21)) #samainīs ar vietām liekot pirmo, kas ir pirmais
---
+++ dalījums_atlikums.py
@@ -0,0 +1,5 @@
+def dalAtl(a,b):
+    return a/b, a//b, a%b
+
+da= dalAtl(13,5)
+print('Dalījuma veselā daļa un atlikums ', da) #izvadīs (2.6, 2, 3), ko sauc par sarakstu. 
