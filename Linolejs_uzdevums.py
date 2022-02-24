d679614759589f8ecf04f26bcfa13143f4288bd9
Kristapinoo <kristapskostukevics@gmail.com>
2022-02-24 16:06:35 +0200

    24.02
    Mājasdarbs

--- Linolejs_uzdevums.py
+++ Linolejs_uzdevums.py
@@ -1,6 +1,3 @@
-import imp
-
-
 import math
 def maksa(vajadzigais):
     cena = vajadzigais * linolejs
@@ -10,10 +7,10 @@ print("Ievadiet lūdzu skaitļus metros un decimālskaitļu starpā liekat '.'")
 a = float(input("Ievadi istabas garumu\n"))
 b = float(input("Ievadi istabas platumu\n"))
 laukums= a * b
-vajadzigais = laukums
-pari = vajadzigais - laukums
-linolejs = float(input('Ievadi linoleja cenu m2, kāds ir veikalā\n'))
-print("Jums būs nepieciešams" ,laukums, "m2 linoleja")
-print('Kopējām izmaksām vajadzētu būt ' + str(math.ceil(maksa(laukums))))
-print('Pāri no nopirktā paliks', pari)
+vajadzigais = round(math.ceil(laukums),2)
+pari = round(math.ceil(laukums),2) - laukums
+linolejs = float(input('Ievadi linoleja cenu m2, kāds ir veikalā.\n'))
+print("Jums būs nepieciešams" ,round(laukums, 2), "m2 linoleja. Sanāk jums būs jāpērk", round(math.ceil(laukums),2), "m2")
+print('Kopējām izmaksām vajadzētu būt', round(maksa(vajadzigais), 2), 'EUR')
+print('Pāri no nopirktā linoleja būs', round(pari, 2), 'm2')
 
