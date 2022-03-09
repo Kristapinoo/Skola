f205a4e55833e685ff9fd8a4472e0befe5c08990
Kristapinoo <kristapskostukevics@gmail.com>
2022-03-09 21:36:52 +0200

    09.03(update)
    Pabeidzu PingPong failu

--- pingpong.py
+++ pingpong.py
@@ -1,10 +1,16 @@
+# Ja vēlas ielikt skaņu, tad tajai skaņai vajag būt vienā folder ar failu
 import turtle
+import winsound   # Atļauj veikt darbības ar operētājsistēmu
 
 screen = turtle.Screen()
 screen.title("PongPing") # loga nosaukums
 screen.bgcolor("black") # loga fona krāsa
 screen.setup(800, 600) # loga izmērs
 screen.tracer(0) #pāatrināt spēli varēs
+
+#Rezultāts
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
@@ -14,6 +20,7 @@ paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
+
 # Paddle B
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
@@ -30,8 +37,17 @@ bumba.shape("square")
 bumba.color("white")
 bumba.penup()
 bumba.goto(0, 0)
-bumba.dx = 0.2     # delta kustības ziņā
-bumba.dy = 0.2
+bumba.dx = 0.1     # delta kustības ziņā
+bumba.dy = 0.1
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal")) # teksts, kuru rādīs, centrā, ar custom fontu, izmēru un stilu(bold, italic)
 
 
 def paddle_a_up():
@@ -75,11 +91,33 @@ while True:
     if bumba.ycor() > 290:
         bumba.sety(290)
         bumba.dy *= -1
+        #winsound.PlaySound("faila_nosaukums.formāts", winsound.SND_ASYNC) # Skaņas ievietošana, aiz faila tas kods nozīmē, ka skaņai skanot viss neapstāsies
     
     if bumba.ycor() < -290:
         bumba.sety(-290)
         bumba.dy *= -1
+        #winsound.PlaySound("faila_nosaukums.formāts", winsound.SND_ASYNC)
 
     if bumba.xcor() > 390:
-        bumba.goto(0)
+        bumba.goto(0, 0)
+        bumba.dx *= -1
+        score_a += 1
+        pen.clear()     # izdzēsīs iepriekšējo cipari, savādāk zīmē pāri tam
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal")) # Šajā gadījumā ar to .format atjaunos rezultātu pēc iegūtā punkta
+        #winsound.PlaySound("faila_nosaukums.formāts", winsound.SND_ASYNC)
+
+    if bumba.xcor() < -390:
+        bumba.goto(0, 0)
+        bumba.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        #winsound.PlaySound("faila_nosaukums.formāts", winsound.SND_ASYNC)
+
+    if bumba.xcor() > 340 and bumba.xcor() < 350 and (bumba.ycor() < paddle_b.ycor() + 40 and bumba.ycor() > paddle_b.ycor() -40): #norāda, laukumu pret kuru atsitīsies pilnā garumā
+        bumba.setx(340)
+        bumba.dx *= -1
+
+    if bumba.xcor() < -340 and bumba.xcor() > -350 and (bumba.ycor() < paddle_a.ycor() + 40 and bumba.ycor() > paddle_a.ycor() -40):
+        bumba.setx(-340)
         bumba.dx *= -1
