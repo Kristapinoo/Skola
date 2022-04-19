a60b8f03bbf09a1a730575c731f3e70bc6bee9f6
Kristapinoo <kristapskostukevics@gmail.com>
2022-04-19 13:23:31 +0300

    19.04
    Klases pabeigtas

--- klases.py
+++ klases.py
@@ -50,15 +50,64 @@
 
 # PIEKĻUVE SLĒPTAJAM LAUKAM SPECIFISKI VAI NETIEŠI
 
+# class person:
+#     def set(self,name,age):
+#         self.__name = name
+#         self.__age = age
+#     def output(self):
+#         print(self.__name,self.__age)
+
+# p = person()
+# p.set("Peter", 20)
+# print(p._person__name) # var piekļūt slēptajam laukam šādi
+# p.output() # var piekļūt slēptajam laukam caur metodi
+
+# class person:
+#     def __init__(self, name, age): # konstruktora definēšana
+#         self.name = name
+#         self.age = age
+#     def output(self):
+#         print(self.name, self.age)
+
+# p = person("Peter", 20)
+
+# p.output()
+
+# class person:
+#     def __init__(self, name, age): # konstruktora definēšana
+#         self.name = name
+#         self.age = age
+#     def output(self):
+#         print(self.name, self.age)
+#     def __del__(self):
+#         print("Deleted:", self.name, self.age)
+
+# p = person("Peter", 20)
+
+# p.output()
+# del p # objekta likvidēšana un attiecīgi destruktora izsaukšana
+
 class person:
-    def set(self,name,age):
-        self.__name = name
-        self.__age = age
-    def output(self):
-        print(self.__name,self.__age)
+    counter = 0 # klases līmeņa lauks
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        person.counter += 1 # klases lauka modifikācija
+    def __del__(self):
+        person.counter -= 1 # klases laika modifikācija
+    def output(self): # instances metode
+        print(self.name, self.age)
+    @classmethod
+    def output_counter(cls): # klases līmeņa metode
+        print(cls.counter)
 
-p = person()
-p.set("Peter", 20)
-print(p._person__name) # var piekļūt slēptajam laukam šādi
-p.output() # var piekļūt slēptajam laukam caur metodi
-
+person.output_counter() # objektu skaits 0
+p1 = person("Liz", 19)
+p1.output()
+p1.output_counter() # objektu skaits 1
+p2 = person("Peter", 20)
+p2.output()
+p2.output_counter() # objektu skaits 2
+p1 = None
+p2 = None
+person.output_counter() # objektu skaits 0
